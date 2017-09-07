b25f6659fe2f03fb1d837deb5b99e64aa918ea16
staaln <staaln@ifi.uio.no>
2017-09-07 11:32:04 +0200

    first commit

---
+++ R/test_func.R
@@ -0,0 +1,12 @@
+#' A Cat Function
+#'
+#' This function allows you to express your love of cats.
+#' @param love Do you love cats? Defaults to TRUE.
+#' @keywords cats
+#' @export
+#' @import pathifier
+#' @examples
+#' cat_function()
+
+
+test_func<-function(x=c(1,2,3)) mean(x)
